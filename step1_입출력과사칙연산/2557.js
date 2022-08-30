7be4a7e2674923c0cf30bfdc6655639cef5ed8f2
joseph-wee <starlightkor@gmail.com>
2022-08-30 18:45:20 +0900

    Hello World

---
+++ step1_입출력과사칙연산/2557.js
@@ -0,0 +1,13 @@
+// 문제
+// Hello World!를 출력하시오.
+
+// 입력
+// 없음
+
+// 출력
+// Hello World!를 출력하시오.
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+console.log("Hello World!")
