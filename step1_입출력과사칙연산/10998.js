0a0b11582c791237e6168c046385bc86793b78d0
joseph-wee <starlightkor@gmail.com>
2022-08-30 18:46:39 +0900

    AxB

---
+++ step1_입출력과사칙연산/10998.js
@@ -0,0 +1,17 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 첫째 줄에 A×B를 출력한다.
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+
+let a = Number(input[0].split(' ')[0]);
+let b = Number(input[0].split(' ')[1]);
+let sum = a*b;
+console.log(sum);
