6a18a59e00ca5ad403e94f9b821870143d86453d
joseph-wee <starlightkor@gmail.com>
2022-08-31 19:11:51 +0900

    고양이

---
+++ step1_입출력과사칙연산/10171.js
@@ -0,0 +1,14 @@
+// 문제
+// 아래 예제와 같이 고양이를 출력하시오.
+
+// 입력
+// 없음.
+
+// 출력
+// 고양이를 출력한다.
+
+
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString();
+
+console.log("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|")
